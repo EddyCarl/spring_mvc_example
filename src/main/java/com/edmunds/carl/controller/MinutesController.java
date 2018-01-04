1b882094a686ec6360aeeae812d25bc0e37afa7c
Carl.Edmunds <carledmunds90@googlemail.com>
2018-01-04 13:11:19 +0000

    Adding JSON view resolver and displaying some JSON
    - Updating some dependencies in the Maven "pom.xml" (previous was an outdated version)
    - Adding a new servlet-mapping for JSON URLs in "web.xml"
    - Adding a new ViewResolver to "servlet-config.xml" for JSON & XML content
    - Created a new "Activity" model POJO
    - Added a method to return some activities as a list

--- src/main/java/com/edmunds/carl/controller/MinutesController.java
+++ src/main/java/com/edmunds/carl/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package com.edmunds.carl.controller;
 
+import com.edmunds.carl.model.Activity;
 import com.edmunds.carl.model.Exercise;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * Gets picked up as a controller due to it being tagged as so and
@@ -19,5 +25,29 @@ public class MinutesController
     {
         System.out.println("Printing exercise value: " + exercise.getMinutes());
         return "addMinutes";
+    }
+
+    @RequestMapping(value = "/activities", method = RequestMethod.GET)
+    public @ResponseBody List<Activity> findAllActivities()
+    {
+        List<Activity> activities = new ArrayList<Activity>();
+
+        Activity run = new Activity();
+        run.setDescription("Run");
+        activities.add(run);
+
+        Activity walk = new Activity();
+        walk.setDescription("Walk");
+        activities.add(walk);
+
+        Activity cycle = new Activity();
+        cycle.setDescription("Cycle");
+        activities.add(cycle);
+
+        Activity swim = new Activity();
+        swim.setDescription("Swim");
+        activities.add(swim);
+
+        return activities;
     }
 }
